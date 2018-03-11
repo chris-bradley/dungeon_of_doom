340e713c8f7af1799b1670e2d911a756d22d91b7
Chris "The Professor" Bradley <chris@theprofessor.ca>
2018-03-10 19:02:56 -0700

    Part of #5: Renamed 'BG' to 'background_colour' in dungeon_generator.c

--- src/dungeon_generator.c
+++ src/dungeon_generator.c
@@ -3,8 +3,8 @@
 
 void lines230_270(int X, int Y, int R[16][16], char *I$, int IX, int IY,
                   int CO);
-void lines280_350(screen_t *screen, enum ColourNum BG, enum ColourNum FG,
-                  int T, int L, int LW);
+void lines280_350(screen_t *screen, enum ColourNum background_colour,
+                  enum ColourNum FG, int T, int L, int LW);
 void lines360_420(screen_t *screen, int W, const char * H$[10]);
 void lines430_440();
 void lines450_600(screen_t *screen, int W, int *LE, int OS, int R[16][16],
@@ -135,8 +135,8 @@ void lines230_270(int X, int Y, int R[16][16], char *I$, int IX, int IY,
 }
 
 
-void lines280_350(screen_t *screen, enum ColourNum BG, enum ColourNum FG,
-                  int T, int L, int LW) {
+void lines280_350(screen_t *screen, enum ColourNum background_colour,
+                  enum ColourNum FG, int T, int L, int LW) {
     // 280 PRINT tab(0,T);
     tab(screen->cursor, 0, T);
     // 290 paper FG:PRINT LEFT$(B$,LW+2)
@@ -144,14 +144,14 @@ void lines280_350(screen_t *screen, enum ColourNum BG, enum ColourNum FG,
     print_left$_b$(screen, LW + 2);
     newline(screen->cursor);
     // 300 paper BG:ink FG
-    paper(screen->cursor, BG);
+    paper(screen->cursor, background_colour);
     ink(screen->cursor, FG);
     // 310 FOR I=1 TO L
     for (int I = 1; I <= L; I +=1) {
     // 320 PRINT BG$(FG);" ";BG$(BG);LEFT$(B$,LW);BG$(FG);" "
         paper(screen->cursor, FG);
         free(print_text(screen, " "));
-        paper(screen->cursor, BG);
+        paper(screen->cursor, background_colour);
         print_left$_b$(screen, LW);
         paper(screen->cursor, FG);
         free(print_text(screen, " "));

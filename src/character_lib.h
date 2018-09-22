2d83cbac2efb46901443bf986adae8fc58e22f5e
Chris "The Professor" Bradley <chris@theprofessor.ca>
2018-09-22 16:32:35 -0600

    Part of #10: Moved the InventoryCode enum to character_lib.h.

--- src/character_lib.h
+++ src/character_lib.h
@@ -8,3 +8,30 @@ enum AttrNum {
     AURA = 6,
     MORALITY = 7
 };
+
+enum InventoryCode {
+    TWO_HAND_SWORD = 0,
+    BROADSWORD = 1,
+    SHORTSWORD = 2,
+    AXE = 3,
+    MACE = 4,
+    FLAIL = 5,
+    DAGGER = 6,
+    GAUNTLET = 7,
+    HEAVY_ARMOUR = 8,
+    CHAIN_ARMOUR = 9,
+    LEATHER_ARMOUR = 10,
+    HEAVY_ROBE = 11,
+    GOLD_HELMET = 12,
+    HEADPIECE = 13,
+    SHIELD = 14,
+    TORCH = 15,
+    NECRONOMICON = 16,
+    SCROLLS = 17,
+    RING = 18,
+    MYSTIC_AMULET = 19,
+    SASH = 20,
+    CLOAK = 21,
+    HEALING_SALVE = 22,
+    POTIONS = 23
+};
--- src/game_module.c
+++ src/game_module.c
@@ -17,33 +17,6 @@ enum CharCode {
     SAFE_PLACE = 110
 };
 
-enum InventoryCode {
-    TWO_HAND_SWORD = 0,
-    BROADSWORD = 1,
-    SHORTSWORD = 2,
-    AXE = 3,
-    MACE = 4,
-    FLAIL = 5,
-    DAGGER = 6,
-    GAUNTLET = 7,
-    HEAVY_ARMOUR = 8,
-    CHAIN_ARMOUR = 9,
-    LEATHER_ARMOUR = 10,
-    HEAVY_ROBE = 11,
-    GOLD_HELMET = 12,
-    HEADPIECE = 13,
-    SHIELD = 14,
-    TORCH = 15,
-    NECRONOMICON = 16,
-    SCROLLS = 17,
-    RING = 18,
-    MYSTIC_AMULET = 19,
-    SASH = 20,
-    CLOAK = 21,
-    HEALING_SALVE = 22,
-    POTIONS = 23
-};
-
 typedef struct {
     float coord_x;
     float coord_y;
